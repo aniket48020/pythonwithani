5b53c9057744a25790f6d5240fca07a4f726aee9
aniket48020 <81038933+aniket48020@users.noreply.github.com>
2021-03-28 22:27:52 +0530

    Create input_array_using_list.py

---
+++ input_array_using_list.py
@@ -0,0 +1,10 @@
+#import numpy as np
+a=[]
+for i in range(0,5):
+    b=int(input('no'))
+    a.append(b)
+#c=np.array(a)
+#d=np.arange(1,10,2)    #(starting point,end point,no of increment)
+#print(d)
+#print(c)
+print(a)
